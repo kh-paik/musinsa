6aa625ffdaa80110d32050091735fe0f99232810
kh-paik <kyunghoon_paik@kakao.com>
2022-01-09 18:58:08 +0900

    Schema 및 데이터 삽입문 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+ALTER TABLE TB_CAT_SUB DROP CONSTRAINT tb_cat_sub_fk;
+
 DROP TABLE IF EXISTS TB_CAT_META;
 CREATE TABLE TB_CAT_META
 (
@@ -5,16 +7,26 @@ CREATE TABLE TB_CAT_META
     category_name VARCHAR(30)
 );
 
-DROP TABLE IF EXISTS TB_CAT_TOP;
-CREATE TABLE TB_CAT_TOP
+DROP TABLE IF EXISTS TB_CAT_SUB;
+CREATE TABLE TB_CAT_SUB
 (
-    sub_category_id INT PRIMARY KEY,
-    sub_category_name VARCHAR(30)
+    category_id INT,
+    sub_category_id INT,
+    sub_category_name VARCHAR(30),
+    CONSTRAINT tb_cat_sub_pk PRIMARY KEY (category_id, sub_category_id),
+    CONSTRAINT tb_cat_sub_fk FOREIGN KEY (category_id) REFERENCES TB_CAT_META (category_id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS TB_CAT_OTR;
-CREATE TABLE TB_CAT_OTR
-(
-    sub_category_id INT PRIMARY KEY,
-    sub_category_name VARCHAR(30)
-);
+--DROP TABLE IF EXISTS TB_CAT_TOP;
+--CREATE TABLE TB_CAT_TOP
+--(
+--    sub_category_id INT PRIMARY KEY,
+--    sub_category_name VARCHAR(30)
+--);
+
+--DROP TABLE IF EXISTS TB_CAT_OTR;
+--CREATE TABLE TB_CAT_OTR
+--(
+--    sub_category_id INT PRIMARY KEY,
+--    sub_category_name VARCHAR(30)
+--);
